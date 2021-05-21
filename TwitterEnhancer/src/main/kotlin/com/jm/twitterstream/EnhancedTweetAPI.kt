1c12f35b274b18879eb8d7c459d1767efb8b1469
James Millner <james_millner@FI10002181.local>
2021-05-21 17:34:54 +0100

    Optimised imports.

--- TwitterEnhancer/src/main/kotlin/com/jm/twitterstream/EnhancedTweetAPI.kt
+++ TwitterEnhancer/src/main/kotlin/com/jm/twitterstream/EnhancedTweetAPI.kt
@@ -5,7 +5,6 @@ import com.jm.twitterstream.dto.EnhancedTweet
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import com.fasterxml.jackson.annotation.JsonProperty
 
 
 @RestController
