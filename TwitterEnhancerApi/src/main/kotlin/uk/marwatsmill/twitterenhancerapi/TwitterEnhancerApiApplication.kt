60a9fc7606bd894742c8d3ba6b691a6ea8a36a84
Andy Marwood <andrew.marwood@skybettingandgaming.com>
2021-06-04 17:42:57 +0100

    Added a new API module

---
+++ TwitterEnhancerApi/src/main/kotlin/uk/marwatsmill/twitterenhancerapi/TwitterEnhancerApiApplication.kt
@@ -0,0 +1,11 @@
+package uk.marwatsmill.twitterenhancerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TwitterEnhancerApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TwitterEnhancerApiApplication>(*args)
+}
