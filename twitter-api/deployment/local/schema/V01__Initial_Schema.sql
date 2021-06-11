68ba4e08e4f753dd5a404a4216b2ac069bd40d28
Matthew Watson <matthew.watson@skybettingandgaming.com>
2021-06-11 18:12:50 +0100

    MYSQL syntax

--- twitter-api/deployment/local/schema/V01__Initial_Schema.sql
+++ twitter-api/deployment/local/schema/V01__Initial_Schema.sql
@@ -13,5 +13,8 @@ CREATE TABLE IF NOT EXISTS tweet (
 );
 
 
-    INSERT INTO tweet VALUES (12341, 23, 69, "test1", "test2", FALSE, "test3", "test4", "tug" );
-    INSERT INTO tweet VALUES (99232, 42, 74, "test3", "test4", TRUE, "test5", "test6", "yey" );
+    INSERT INTO tweet
+        (id, author_id, conversation_id, created_at, lang, possible_sensitive, reply_settings, source, text, last_updated)
+        VALUES
+               (12341, 23, 69, 'test1', 'test2', FALSE, 'test3', 'test4', 'tug' ),
+               (99232, 42, 74, 'test3', 'test4', TRUE, 'test5', 'test6', 'yey' );
